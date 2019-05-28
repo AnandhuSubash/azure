1438cbd7e1df0aca48cbd24451a23b94a20e13ab
AnandhuSubash <45092230+AnandhuSubash@users.noreply.github.com>
2019-05-28 11:40:47 +0530

    Update fmart.php

--- fmart.php
+++ fmart.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>Fmart</h1>
-  </body>
-</html>
+<?php
+echo "fmart";
+?>
